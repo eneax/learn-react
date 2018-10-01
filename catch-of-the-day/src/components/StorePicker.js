b664f582fda04d6c4dd2fbf460e4255599aa540c
eneax <19818937+eneax@users.noreply.github.com>
2018-10-01 08:16:51 +0200

    Create StorePicker component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  // every class needs at least one method inside
+  render() {
+    return <p>I'm the store Picker!</p>
+  }
+}
+
+export default StorePicker;
