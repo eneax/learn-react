04b980e1bae9a477550b5f0083ffaf7fbdcdfc02
eneax <19818937+eneax@users.noreply.github.com>
2018-10-02 20:55:30 +0200

    Write HTML with JSX

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,8 +3,31 @@ import React from 'react';
 class StorePicker extends React.Component {
   // every class needs at least one method inside
   render() {
-    return <p>I'm the store Picker!</p>
+    // the return method can return only one element
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store ➡</button>
+      </form>
+    )
   }
 }
 
 export default StorePicker;
+
+
+
+/*
+use React.Fragment, so you won't have all those divs
+
+<React.Fragment>
+  <p>Fish</p>
+  <form className="store-selector">
+    <h2>Please Enter A Store</h2>
+    <input type="text" required placeholder="Store Name" />
+    <button type="submit">Visit Store</button>
+  </form>
+</React.Fragment>
+
+*/ 
