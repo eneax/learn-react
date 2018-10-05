89f7323adb6515f6bb64dfac4e68309fcf46af62
eneax <19818937+eneax@users.noreply.github.com>
2018-10-05 07:56:36 +0200

    Add Helper and Utility Functions

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   // every class needs at least one method inside
@@ -7,7 +8,7 @@ class StorePicker extends React.Component {
     return (
       <form className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store ➡</button>
       </form>
     )
