fd18e66a298e9670e1a9a3a5c31db80f20b1dfd7
eneax <19818937+eneax@users.noreply.github.com>
2018-10-06 14:31:22 +0200

    Events, Refs and this Binding

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,27 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  // every class needs at least one method inside
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from that input
+    console.log(this);
+    // 3. Change the store to /store/whatever-they-entered
+    
+  }
   render() {
-    // the return method can return only one element
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input 
+          type="text"
+          ref={this.myInput}
+          required 
+          placeholder="Store Name" 
+          defaultValue={getFunName()} 
+        />
         <button type="submit">Visit Store ➡</button>
       </form>
     )
@@ -17,6 +31,14 @@ class StorePicker extends React.Component {
 
 export default StorePicker;
 
+
+/* 
+* getFunName() --> runs immediately as soon as it mounts 
+<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+
+* ref --> allows us to reference a DOMNode on the page without using document.querySelector()
+* In React, methods that we create need to be bind to our custom Components
+*/
 
 
 /*
