8eec5fd3b4c0294bd4dea2b7b09c8a8f05c8f053
eneax <19818937+eneax@users.noreply.github.com>
2018-12-09 11:19:48 +0100

    Add JSX and CSS into StorePicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,11 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-      <p>I'm a StorePicker!</p>
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store ➡</button>
+      </form>
     );
   }
 }
