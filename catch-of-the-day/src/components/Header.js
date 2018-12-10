f3582f40a60654b89a6409a3e2b77ebb0051c172
eneax <19818937+eneax@users.noreply.github.com>
2018-12-10 08:48:07 +0100

    Intro to props and Stateless functional components

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,5 +1,30 @@
 import React from 'react';
 
+
+// Stateless functional component 
+const Header = (props) => (
+  <header className="top">
+    <h1>
+      Catch
+        <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+      </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+
+export default Header;
+
+
+
+
+/*
+
 class Header extends React.Component {
   render() {
     return (
@@ -13,11 +38,11 @@ class Header extends React.Component {
           Day
         </h1>
         <h3 className="tagline">
-          <span>Fresh Daily</span>
+          <span>{this.props.tagline}</span>
         </h3>
       </header>
     );
   }
 }
 
-export default Header;
+*/
